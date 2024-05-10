4c613082732572c65ddde50f30a29174a92d8030
GunjanAgarwal123 <gunagarwal126483@gmail.com>
2024-05-10 23:22:41 +0530

    Add files via upload
    static pattern created

---
+++ staticPattern/SwsasticPattern.java
@@ -0,0 +1,16 @@
+class SwsasticPattern{
+	public static void main(String[] args){
+		for(int i=0;i<=6;i++){
+			for(int j=0;j<=8;j++){
+				if((i==3) || (j==4) || (i<3 && j==0) || (i==0 && j>4) || (i==6 && j<4) || (i>3 && j==8)){
+					System.out.print("* ");
+					}
+				else{
+					
+					System.out.print("  ");
+					}
+				}
+			System.out.println();
+			}
+		}
+	}
